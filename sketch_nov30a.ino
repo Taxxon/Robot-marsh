527582412c76940d6034f5a900643bbfdc0d4b9e
Taxxon <te15.emil.kack@umea.nti.se>
2016-11-30 13:38:31 +0100

    Add files via upload
    Märker att det kör in i en vägg.

---
+++ sketch_nov30a.ino
@@ -0,0 +1,15 @@
+void setup() {
+Serial.begin(9600);
+pinMode(5, INPUT);
+pinMode(6, INPUT);
+ 
+
+
+}
+
+void loop() {
+ byte wLeft = digitalRead(5);
+ byte wRight = digitalRead(6);
+ Serial.print(wLeft);
+ Serial.print(wRight);
+}
