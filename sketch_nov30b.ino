5934aec92d1fdb10ca7c54612d3744dcb52a69d3
Taxxon <te15.emil.kack@umea.nti.se>
2016-11-30 14:39:03 +0100

    Add files via upload
    Test för ljus detektorn

---
+++ sketch_nov30b.ino
@@ -0,0 +1,14 @@
+void setup() {
+Serial.begin(9600);
+
+}
+double volts(int adPin) {           // Vad den får för inputt
+  return double(analogRead(adPin)); // Returnar inputt
+}
+void loop() {
+ Serial.print("A3 = ");             // Printar ut A3 = 
+ Serial.println(volts(0));          //Printar ut hur ljust det är
+ delay(1000);                       // Delay innan den printar ut
+}
+
+//Test för ljus detektorn. Om det är ljust så blir värdet högre.
